3fa81f3c06afaea035dd3078d25353a78f0fc183
justin rebbeck <jrebbeck2bsd220.org>
2023-11-28 09:39:09 -0600

    Initial Commit

---
+++ Pig try2/Pig_try2App.swift
@@ -0,0 +1,17 @@
+//
+//  Pig_try2App.swift
+//  Pig try2
+//
+//  Created by Justin Rebbeck on 11/28/23.
+//
+
+import SwiftUI
+
+@main
+struct Pig_try2App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
